90dead22828e3de0db8e236a82c0ded0b0253cc1
M4ximich <65494293+M4ximich@users.noreply.github.com>
2020-10-29 16:11:21 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,12 @@ const loginInput=document.querySelector('#login')
 const userName=document.querySelector('.user-name')
 const buttonOut=document.querySelector('.button-out')
 const cardsRestaurants=document.querySelector('.cards-restaurants')
+const containerPromo=document.querySelector('.container-promo')
+const restaurants=document.querySelector('.restaurants')
+const menu=document.querySelector('.menu')
+const logo=document.querySelector('.logo')
+const cardsMenu=document.querySelector('.cards-menu')
+
 function toggleModalAuth(){
   modalAuth.classList.toggle('is-open')
 }
@@ -63,14 +69,14 @@ function checkAuth(){
 }
 else{
   notAuthorized();
-  if (login){authorized}else{loginInput.style.backgroundColor=rgb(0,0,0)}
+  if (login){authorized}else{loginInput.style.backgroundColor='red'}
 }
 }
 checkAuth()
 //day2
 function createCardRestaurant(){
   const card=`
-    <a href="restaurant.html" class="card card-restaurant">
+    <a class="card card-restaurant">
       <img src="img/tanuki/preview.jpg" alt="image" class="card-image"/>
       <div class="card-text">
         <div class="card-heading">
@@ -89,8 +95,54 @@ function createCardRestaurant(){
       </div>
       <!-- /.card-text -->
     </a>`
-  cardsRestaurants.insertAdjacentHTML('afterend', card)
+  cardsRestaurants.insertAdjacentHTML('beforeend', card)
 }
-createCardRestaurant();
 createCardRestaurant()
 createCardRestaurant()
+function createCardGood(){
+  const card=document.createElement('div')
+  card.className='card'
+  card.insertAdjacentHTML('beforeend', `
+          <div class="card">
+						<img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image"/>
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">Пицца Везувий</h3>
+							</div>
+							<!-- /.card-heading -->
+							<div class="card-info">
+								<div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+									«Халапенье», соус «Тобаско», томаты.
+								</div>
+							</div>
+							<!-- /.card-info -->
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">545 ₽</strong>
+							</div>
+						</div>
+						<!-- /.card-text -->
+					</div>
+          <!-- /.card -->`)
+          cardsMenu.insertAdjacentElement('beforeend', card)
+}
+function openGoods(event){
+  const target=event.target
+  const restaurant=target.closest('.card-restaurant')
+  if (restaurants){
+    containerPromo.classList.add('hide')
+    restaurants.classList.add('hide')
+    menu.classList.remove('hide')
+    cardsMenu.textContent=''
+    createCardGood()
+  }
+}
+cardsRestaurants.addEventListener('click', openGoods)
+logo.addEventListener('click', function(){
+  containerPromo.classList.remove('hide')
+  restaurants.classList.remove('hide')
+  menu.classList.add('hide')
+})
